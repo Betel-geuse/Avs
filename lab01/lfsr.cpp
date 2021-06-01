6b3940620bae3c3a501a1d3609a5b990c928380e
Betel-geuse <serega1357912@gmail.com>
2021-06-02 02:26:07 +0300

    done 1 lab

--- lab01/lfsr.cpp
+++ lab01/lfsr.cpp
@@ -2,6 +2,10 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+    char lbit = (*reg >> 5) ^ (*reg >> 3) ^
+        (*reg >> 2) ^ (*reg & 1);
+
+    *reg = *reg >> 1;
+    *reg = (*reg & ~(1 << 15)) | (lbit << 15);
 }
 
--- lab01/ll_cycle.cpp
+++ lab01/ll_cycle.cpp
@@ -1,6 +1,17 @@
 #include "ll_cycle.h"
+#define NULL 0
+bool ll_has_cycle(node* head) {
 
-bool ll_has_cycle(node *head) {
-    /* Ваш код должен быть написан только внутри этой функции */
-    return true;
+    node* slow = head, * fast = head;
+
+    while (slow != NULL && fast != NULL && fast->next != NULL) {
+
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+            return true;
+    }
+
+    return false;
 }
